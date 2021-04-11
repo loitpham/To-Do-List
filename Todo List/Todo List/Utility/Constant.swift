e4166d1c5de0b814ad855bef3cea922c2af615e2
Loi Pham <loitpham@gmail.com>
2021-04-10 20:42:08 -0700

    Reorganized the default code provided by Core Data

---
+++ Todo List/Todo List/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Todo List
+//
+//  Created by Loi Pham on 4/10/21.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
+
