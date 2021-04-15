d61e4fd4cc99b04a79e1f1489109399f53da32d2
Loi Pham <loitpham@gmail.com>
2021-04-14 20:24:54 -0700

    Added a custom header.

--- Todo List/Todo List/View/ContentView.swift
+++ Todo List/Todo List/View/ContentView.swift
@@ -11,6 +11,7 @@ import CoreData
 struct ContentView: View {
     // MARK: - PROPERTIES
 
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @State var task: String = ""
     @State private var showNewTaskItem: Bool = false
     
@@ -46,6 +47,40 @@ struct ContentView: View {
                 // MARK: - MAIN VIEW
                 VStack {
                     // MARK: - HEADER
+                    
+                    HStack(spacing: 10) {
+                        // TITLE
+                        Text("Todo List")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding(.leading, 4)
+                        
+                        Spacer()
+                        
+                        // EDIT BUTTON
+                        EditButton()
+                            .font(.system(size: 16, weight: .semibold, design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(
+                                Capsule().stroke(Color.white, lineWidth: 2)
+                            )
+                        
+                        // APPEARANCE BUTTON
+                        Button(action: {
+                            // TOGGLE APPEARANCE
+                            isDarkMode.toggle()
+                        }) {
+                            Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle.fill")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
+                        }
+                        
+                    } //: HSTACK
+                    .padding()
+                    foregroundColor(.white)
+                    
                     
                     Spacer(minLength: 80)
                     
@@ -107,13 +142,7 @@ struct ContentView: View {
                 UITableView.appearance().backgroundColor = UIColor.clear
             }
             .navigationBarTitle("Daily Tasks", displayMode: .large)
-            .toolbar {
-                #if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                #endif
-            } //: TOOLBAR
+            .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
             )
--- Todo List/Todo List/View/NewTaskItemView.swift
+++ Todo List/Todo List/View/NewTaskItemView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct NewTaskItemView: View {
     // MARK: - PROPERTIES
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @Environment(\.managedObjectContext) private var viewContext
     @State private var task: String = ""
     @Binding var isShowing: Bool
@@ -47,11 +48,12 @@ struct NewTaskItemView: View {
             Spacer()
             VStack(spacing: 16) {
                 TextField("New Task", text: $task)
+                    .autocapitalization(.none)
                     .foregroundColor(.pink)
                     .font(.system(size: 24, weight: .bold, design: .rounded))
                     .padding()
                     .background(
-                        Color(UIColor.systemGray6)
+                        isDarkMode ? Color(UIColor.tertiarySystemBackground) : Color(UIColor.secondarySystemBackground)
                     )
                     .cornerRadius(10)
                 
@@ -72,7 +74,7 @@ struct NewTaskItemView: View {
             .padding(.horizontal)
             .padding(.vertical, 20)
             .background(
-                Color.white
+                isDarkMode ? Color(UIColor.secondarySystemBackground) : Color.white
             )
             .cornerRadius(16)
             .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
