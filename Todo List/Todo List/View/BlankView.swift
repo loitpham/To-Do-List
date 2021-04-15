d2edc8fbb6933f2cefe6aa29bc0d1803e6866445
Loi Pham <loitpham@gmail.com>
2021-04-14 22:09:03 -0700

    Added a blur effect when adding a new task.

--- Todo List/Todo List/View/BlankView.swift
+++ Todo List/Todo List/View/BlankView.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct BlankView: View {
+    // MARK: - PROPERTIES
+    
+    var backgroundColor: Color
+    var backgroundOpacity: Double
+    
+    // MARK: - BODY
     var body: some View {
         VStack {
             Spacer()
         }
         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-        .background(Color.black)
-        .opacity(0.5)
+        .background(backgroundColor)
+        .opacity(backgroundOpacity)
+        .blendMode(.overlay)
         .edgesIgnoringSafeArea(.all)
     }
 }
 
+// MARK: - PREVIEW
+
 struct BlankView_Previews: PreviewProvider {
     static var previews: some View {
-        BlankView()
+        BlankView(backgroundColor: Color.black, backgroundOpacity: 0.3)
+            .background(BackgroundImageView())
+            .background(backgroundGradient.ignoresSafeArea(.all))
     }
 }
